dda7c5ce241e3c347982b83fc3933ee9403f4589
shalin.v <shalin.v@spb.helix.ru>
2016-05-17 19:40:32 +0300

    Init project

---
+++ src/HelixLearning/HelloWorldGit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sadMessage = "Hello world, Git!";
+            Console.WriteLine("Nothing works :,(");
+            Console.ReadKey(true);
+        }
+    }
+}
